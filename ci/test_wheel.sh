d63d3237b333fa6247bbf64c75feedc768341c2e
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2023-04-03 19:57:52 -0300

    CI: simplify wheel test
    We don't need extra dependencies here.

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -9,7 +9,6 @@ for pydir in $PYTHON_DIRS
 do
     echo Installing for CPython $pydir
     export PATH=/opt/python/${pydir}/bin/:$ORIGINAL_PATH
-    python -m pip install scipy matplotlib
     python -m pip install artifacts/dss_python_backend-*.whl
     python -c 'from dss_python_backend import lib, ffi; lib.ctx_DSS_NewCircuit(ffi.NULL, b"test123"); assert ffi.string(lib.ctx_Circuit_Get_Name(ffi.NULL)) == b"test123"'
 done
