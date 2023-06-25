987bbb53800d2f9571cb32fefcf44b392d500409
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2023-06-25 20:33:17 -0300

    Add placeholder functions for DSSEvents callbacks

--- cffi/dss_capi_custom.h
+++ cffi/dss_capi_custom.h
@@ -1,3 +1,6 @@
 extern "Python" int32_t dss_python_cb_plot(void *ctx, char* params);
 extern "Python" int32_t dss_python_cb_write(void *ctx, char* messageStr, int32_t messageType);
 
+extern "Python" void dss_python_cb_init_controls(void *ctx);
+extern "Python" void dss_python_cb_check_controls(void *ctx);
+extern "Python" void dss_python_cb_step_controls(void *ctx);
